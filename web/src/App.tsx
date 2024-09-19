149049598e3d5bfbc3540c63c25c4b88423b60eb
Joao Camargo <jclaudio00_12@hotmail.com>
2024-09-19 19:38:05 -0300

    new changes frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,7 +9,12 @@ export function App() {
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
         Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
-      <button>Cadastrar Meta</button>
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tighter hover:bg-violet-600"
+      >
+        Cadastrar Meta
+      </button>
     </div>
   );
 }
